5f6b885f6fb406ca3a9df01cee026bef125ac25f
Basil Gilliam <basilgilliam@gmail.com>
2019-01-18 00:35:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
-function scuberGreetingForFeet (aNumber) {
- if (aNumber <= 400){
+function scuberGreetingForFeet(aNumber) {
+ if(aNumber <= 400){
  return "this one is one me!"
    
  }
