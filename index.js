740674f1772dd6de4346ab5b90b5aac2ecf212e2
Basil Gilliam <basilgilliam@gmail.com>
2019-01-18 00:48:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function scuberGreetingForFeet(aNumber) {
  if(aNumber <= 400){
  return "This one is one me!"
  }else if(aNumber > 2500){
-   return "No can do"
+   return "No can do."
  }else if(aNumber > 2000){
-   return 'This ride is $30'
+   return 'I will gladly take your thirty bucks.'
  }
 }
